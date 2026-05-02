f052b21d86e741c8e12c65177735129855c25c6e
kumro-potash <goswamineelim@outlook.com>
2026-05-03 00:27:00 +0530

    port

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mailRoutes from "./routes/mail.routes.js";
 import addressRoutes from "./routes/addressRoute.js";
+import "./jobs/cleanup.job.js"
 
 dotenv.config();
 
@@ -10,12 +11,11 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// ✅ ADD THIS LINE
 app.use("/api/address", addressRoutes);
 
-// existing
 app.use("/api/webhook", mailRoutes);
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log(`Server running on port 3000`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
