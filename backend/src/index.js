c171cf9f3617523490630fd41b5592868c385212
kumro-potash <goswamineelim@outlook.com>
2026-05-03 01:09:21 +0530

    CORS v1

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import mailRoutes from "./routes/mail.routes.js";
 import addressRoutes from "./routes/addressRoute.js";
 import "./jobs/cleanup.job.js"
-import express from "express";
 import cors from "cors";
 
 const app = express();
@@ -12,8 +11,6 @@ app.use(cors());
 
 app.use(express.json());
 dotenv.config();
-
-const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
